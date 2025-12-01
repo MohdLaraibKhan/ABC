5c4a31b4816fef0e27d8ed6b7afe5b24a3241a56
MohdLaraibKhan <khanlaraibkhan15@gmail.com>
2025-12-01 12:38:28 +0530

    added student class

---
+++ Student.java
@@ -0,0 +1,8 @@
+public class Student {
+    public Student() {
+        System.out.println("Creating student object");
+    }   
+    public void sayHello (){
+        System.out.println("Hii , I am a student");
+    }
+}
